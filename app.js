cfbb809a133a1fa00582932b0273c627c447c334
Talia Zwennis <126955512+taliazwennis@users.noreply.github.com>
2023-03-05 20:35:00 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,59 +1,120 @@
+//jshint esversion:6
+
 const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
 const app = express();
-const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.set("view engine", "ejs");
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
+let day = "";
+mongoose.connect(
+  "mongodb+srv://taliazwennis:Test123@cluster0.tflix2w.mongodb.net/todolistDB"
+);
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+const itemsSchema = { name: String };
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your to do list",
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new line",
+});
+const item3 = new Item({
+  name: "<--- Hit this to delete an item",
+});
+
+const listScheme = {
+  name: String,
+  items: [itemsSchema],
+};
+
+const List = mongoose.model("List", listScheme);
+
+app.get("/", async function (req, res) {
+  day = date.getDate();
+  const foundItems = await Item.find({}).exec();
+  if (foundItems.length === 0) {
+    Item.insertMany([item1, item2, item3])
+      .then(function () {
+        console.log("Successfully saved defult items to DB");
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+  res.render("list", { listTitle: day, newListItems: foundItems });
+
+  res.redirect("/");
+});
+
+app.post("/", async function (req, res) {
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+  const item = new Item({ name: itemName });
+  if (listName === day) {
+    item.save();
+    res.redirect("/");
+  } else {
+    const foundList = await List.findOne({ name: listName }).exec();
+    foundList.items.push(item);
+    foundList.save();
+    res.redirect("/" + listName);
+  }
+});
+
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+  if (listName === day) {
+    Item.findByIdAndRemove(checkedItemId)
+      .then(function () {
+        console.log("Successfully deleted checked item");
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    res.redirect("/");
+  } else {
+    List.findOneAndUpdate(
+      { name: listName },
+      { $pull: { items: { _id: checkedItemId } } }
+    )
+      .then(function () {
+        res.redirect("/" + listName);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+});
+
+app.get("/:customListName", async function (req, res) {
+  const customListName = _.capitalize(req.params.customListName);
+  const foundList = await List.findOne({ name: customListName }).exec();
+  if (foundList == null) {
+    const list = new List({
+      name: customListName,
+      items: [item1, item2, item3],
+    });
+    list.save();
+    res.redirect("/" + customListName);
+  } else {
+    res.render("list", {
+      listTitle: customListName,
+      newListItems: foundList.items,
+    });
+  }
+});
+
+app.listen(3001, function () {
+  console.log("Server started on port 3001");
+});
